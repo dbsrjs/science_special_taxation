e9faf757ef5bf116ac8b9a130e210942cc76c409
dbsrjs <dbsrjs1224@gmail.com>
2024-01-10 19:54:52 +0900

    01/10 science-special-taxation

--- Assets/0.Script/Player.cs
+++ Assets/0.Script/Player.cs
@@ -2,29 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private GameObject gameOver;   //gameOver 메시지
     [SerializeField] private GameObject restart_buttin; //재시작 버튼
-
     [SerializeField] private Animator animator;
 
     private Score score;
 
+    private bool jump = true;
+    private Quaternion initialRotation;
+
     void Start()
     {
         score = FindObjectOfType<Score>();
+        initialRotation = transform.rotation;
     }
 
 
     void Update()
     {
-        if (Time.timeScale != 0 && score.panel.activeSelf == false)
+        transform.rotation = initialRotation;
+
+        if (Time.timeScale != 0 && jump == true)
         {
             animator.SetTrigger("doRun");
-            if (Input.GetKeyDown(KeyCode.Space) && transform.position.y <= 0.017)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
+                jump = false;
                 animator.SetTrigger("doJump");
                 Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
                 rigidbody.AddForce(new Vector2(0f, 6f), ForceMode2D.Impulse);
@@ -37,10 +44,15 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        /*
-        if (score != null)
+        if (collision.collider.tag == "Ground")
+        {
+            Debug.Log("test");
+            jump = true;
+        }
+        
+        else if (score != null)
         {
             if (collision.gameObject.layer == 6)    //선인장과 충돌
             {
@@ -51,12 +63,11 @@ public class Player : MonoBehaviour
                 restart_buttin.SetActive(true);
             }
         }
-        */
     }
-    
+
     public void Restart_Btn()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Game");
         Time.timeScale = 1;
     }
 }
